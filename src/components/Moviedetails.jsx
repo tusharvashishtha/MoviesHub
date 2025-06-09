67c489f5385d0c2d6911926a93bd72debf04e11e
Siddharth6511 <vashishthatushar004@gmail.com>
2025-06-09 18:05:26 +0530

    M

--- src/components/Moviedetails.jsx
+++ src/components/Moviedetails.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { asyncloadmovie, removemovie } from "../store/actions/MovieActions";
 import { Link } from "react-router-dom";
 import Loading from "../components/Loading";
 
 const Moviedetails = () => {
   const navigate = useNavigate();
+  const {pathname} = useLocation();
   const { id } = useParams();
   const { info } = useSelector((state) => state.movie);
   const dispatch = useDispatch();
@@ -69,15 +70,45 @@ const Moviedetails = () => {
           }`}
           alt=""
         />
+
+        <div className="content ml-[5%]">
+          <h1 className="text-5xl font-black text-white">{info.detail.name ||info.detail.original_name  || info.detail.title ||info.detail.original_title}
+          <small className="text-2xl font-bold text-zinc-300">
+            ({info.detail.release_date.split("-")[0]})
+          </small>
+
+          </h1>
+
+          <div className="mt-3 mb-3 flex text-white items-center gap-x-5 ">
+           <span className='bg-yellow-600  text-white font-semibold w-[9vh] h-[4vh] flex justify-center items-center'><i className="mr-[1vh] ri-star-fill"></i>{((info.detail.vote_average * 10).toFixed())/10}
+           </span>
+           <h1 className="font-semibold w-[70px] text-2xl leading-7">Ratings</h1>
+           <h1>{info.detail.release_date}</h1>
+           <h1>{info.detail.genres.map((g) => g.name).join(' , ')}</h1>
+           <h1>{info.detail.runtime}min</h1>
+          </div>
+
+          <h1 className="text-2xl font-semibold italic text-zinc-200">{info.detail.tagline}</h1>
+
+          <h1 className="text-2xl text-white mb-3 mt-5">Overview</h1>
+          <p className="text-xl text-white italic">{info.detail.overview}</p>
+
+          <Link to={`${pathname}/trailer`}>Play Trailer</Link>
+        
+         
+        </div>
       </div>
 
       {/* Part 3 avilavle on  flexPlatforms */}
-      <div className="w-[80%] flex flex-col gap-y-5 mt-10">
+      <div className="w-[30%]  flex flex-col justify-between gap-y-5 mt-10">
         {/* Flatrate Providers */}
     
           {info.watchproviders && info.watchproviders.flatrate && (
             <div className="flex gap-x-10 items-center text-white">
+              <div className=" w-[55%]">
               <h1>Available on Platforms</h1>
+              </div>
+      
               {info.watchproviders.flatrate.map((w, i) => (
                 <img
                 title={w.provider_name}
@@ -92,7 +123,10 @@ const Moviedetails = () => {
 
           {info.watchproviders && info.watchproviders.rent && (
             <div className="flex gap-x-10 items-center text-white">
+              <div className=" w-[55%]">
               <h1>Available on Rent</h1>
+
+              </div>
               {info.watchproviders.rent.map((w, i) => (
                 <img
                 title={w.provider_name}
@@ -104,9 +138,12 @@ const Moviedetails = () => {
               ))}
             </div>
           )}
+
           {info.watchproviders && info.watchproviders.buy && (
             <div className="flex gap-x-10 items-center text-white">
+              <div className=" w-[55%]">
               <h1>Available to Buy</h1>
+              </div>
               {info.watchproviders.buy.map((w, i) => (
                 <img
                   title={w.provider_name}
--- src/store/actions/MovieActions.jsx
+++ src/store/actions/MovieActions.jsx
@@ -9,12 +9,14 @@ export const asyncloadmovie = (id) => async (dispatch , getState) => {
         const recommendations = await axios.get(`/movie/${id}/recommendations`);
         const similar = await axios.get(`/movie/${id}/similar`);
         const videos = await axios.get(`/movie/${id}/videos`);
+        const translations = await axios.get(`/movie/${id}/translations`);
         const watchproviders = await axios.get(`/movie/${id}/watch/providers`);
         let ultimateDetails = {
             detail: detail.data,
             externalid: externalid.data,
             recommendations: recommendations.data.results,
             similar: similar.data.results,
+            translations: translations.data.translations.map((t) => t.name),
             videos: videos.data.results.find(m => m.type === "Trailer"),
             watchproviders: watchproviders.data.results.IN,
         }
